774f6eb6e4929dcc3378c2d8eae9ca25f5241130
chalermsak <chalermsak@dga.or.th>
2023-08-07 15:41:44 +0700

    update k8s

---
+++ k8s/web-demo/testcanary.sh
@@ -0,0 +1,18 @@
+# Define the URL and the filter word
+url="http://gdcc-canary.dg-paas.cloud"
+filter_word="<th colspan=\"2\">"
+
+# Set the loop count to 100
+loop_count=16
+
+# Loop 'loop_count' times
+for ((i=1; i<=loop_count; i++)); do
+    # Execute curl command and grep for the filter word
+    result=$(curl -s -L "$url" | grep -E "$filter_word")
+    sleep 1
+    # Display the output if it matches the filter word
+    if [[ -n "$result" ]]; then
+        echo "Match found in iteration $i:"
+        echo "$result"
+    fi
+done
